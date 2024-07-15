d81bb75ff16bed57bd9d913c9e94158e86b2e779
Eko Bayu <eko.bayu@ogilvy.com>
2024-07-15 11:29:55 +0800

    edit base path nextjs to folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/',
+  basePath: '/ekobayu-profile.github.io/',
   output: 'export'
   // swcMinify: false,
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
