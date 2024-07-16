e09f31b8e41f45a284f7327b80ace36a67f9ef35
Eko Bayu <eko.bayu@ogilvy.com>
2024-07-16 11:07:21 +0800

    add robots, sitemap and opengraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Nextjs Firebase',
+  title: 'Nextjs Firebase Home',
   description: 'Firebase and Nextjs Web Application'
 }
 export default function RootLayout({ children }) {
