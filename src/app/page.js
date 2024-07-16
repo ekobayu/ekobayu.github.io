915ab71d8094e8fb9857adb41f2851acf8e2091a
Eko Bayu <eko.bayu@ogilvy.com>
2024-07-16 11:07:37 +0800

    add sample link routing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 
 import AddItem from '../components/AddItem'
 import ListItems from '../components/ListItems'
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to My Next.js App</h1>
+      <Link href='/work'>Work</Link>
       <AddItem />
       <ListItems />
     </div>
