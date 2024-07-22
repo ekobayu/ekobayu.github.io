9f0212e8c418be81cad14c59f717d7f1617008ac
Eko Bayu <ekobayu@mail.com>
2024-07-22 08:22:02 +0800

    initial react full page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
 'use client'
-import Link from 'next/link'
+import { useState } from 'react'
+import { FullpageContainer, FullpageSection } from '@shinyongjun/react-fullpage'
+import '@shinyongjun/react-fullpage/css'
 
 import AddItem from '../components/AddItem'
 import ListItems from '../components/ListItems'
 
 export default function Home() {
+  const [activeIndex, setActiveIndex] = useState<number>(0)
   return (
-    <div>
-      <h1>Welcome to My Next.js App</h1>
-      <AddItem />
-      <ListItems />
-    </div>
+    <FullpageContainer activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
+      <FullpageSection>
+        <div>
+          <h1>Welcome to My Next.js App</h1>
+          <AddItem />
+          <ListItems />
+        </div>
+      </FullpageSection>
+      <FullpageSection>
+        <div>Section 2</div>
+      </FullpageSection>
+      <FullpageSection>
+        <div>Section 3</div>
+      </FullpageSection>
+      <FullpageSection isAutoHeight>
+        <footer>Footer</footer>
+      </FullpageSection>
+    </FullpageContainer>
   )
 }
