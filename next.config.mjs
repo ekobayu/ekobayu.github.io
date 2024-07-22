8fa53987f1ac08dcc87e6168058a48a3d2f64930
Eko Bayu <eko.bayu@ogilvy.com>
2024-07-22 11:52:49 +0800

    include variable and mixin in global

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // basePath: '/ekobayu-profile.github.io/',
-  output: 'export'
+  output: 'export',
+  sassOptions: {
+    includePaths: ['/'],
+    prependData: `@import "/styles/variables.scss"; @import "/styles/mixin.scss";`
+  }
   // clientRouterFilter: false
   // swcMinify: false,
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
