b546d67419ddde4b366d7a486d9d03f54109a2b3
Eko Bayu <eko.bayu@ogilvy.com>
2024-07-22 15:59:41 +0800

    added bg line section 1 and h1 style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,39 @@
 import { useState } from 'react'
 import { FullpageContainer, FullpageSection } from '@shinyongjun/react-fullpage'
 import '@shinyongjun/react-fullpage/css'
+import bgLine from '../assets/bg-line.svg'
 
-import AddItem from '../components/AddItem'
-import ListItems from '../components/ListItems'
+// import AddItem from '../components/AddItem'
+// import ListItems from '../components/ListItems'
 
 export default function Home() {
   const [activeIndex, setActiveIndex] = useState<number>(0)
   return (
     <FullpageContainer activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
       <FullpageSection>
-        <div>
-          <h1>Welcome to My Next.js App</h1>
-          <AddItem />
-          <ListItems />
+        <div
+          className='section-1'
+          style={{
+            backgroundImage: `url(${bgLine.src})`,
+            width: '100%',
+            minHeight: '500px'
+          }}
+        >
+          <div className='container mx-auto px-5 md:px-0'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+              <div>
+                <h1>Frontend Developer</h1>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem a perspiciatis id nemo quibusdam
+                  placeat asperiores quisquam repellat nesciunt impedit. Nobis optio, inventore necessitatibus commodi
+                  aperiam repellat error ut quibusdam!
+                </p>
+              </div>
+              <div>09</div>
+            </div>
+            {/* <AddItem />
+            <ListItems /> */}
+          </div>
         </div>
       </FullpageSection>
       <FullpageSection>
