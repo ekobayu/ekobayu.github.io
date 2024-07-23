8f548312cfbe34d4744fd8109ce56349cf44fdb1
Eko Bayu <eko.bayu@ogilvy.com>
2024-07-23 11:06:17 +0800

    add section 2 content and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { useState } from 'react'
 import { FullpageContainer, FullpageSection } from '@shinyongjun/react-fullpage'
+import { List } from 'flowbite-react'
+
 import '@shinyongjun/react-fullpage/css'
 import bgLine from '../assets/bg-line.svg'
 
-import AddItem from '../components/AddItem'
-import ListItems from '../components/ListItems'
+// import AddItem from '../components/AddItem'
+// import ListItems from '../components/ListItems'
 
 export default function Home() {
   const [activeIndex, setActiveIndex] = useState<number>(0)
@@ -20,26 +22,54 @@ export default function Home() {
             minHeight: '500px'
           }}
         >
-          <div className='container mx-auto px-5 md:px-0'>
+          <div className='container mx-auto px-5 md:px-0 my-32 md:my-60'>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
               <div>
                 <h1>Frontend Developer</h1>
                 <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem a perspiciatis id nemo quibusdam
-                  placeat asperiores quisquam repellat nesciunt impedit. Nobis optio, inventore necessitatibus commodi
-                  aperiam repellat error ut quibusdam!
+                  A results-driven individual with expertise in both front-end development and SEO, passionate about
+                  creating exceptional online experiences that drive traffic and engagement.
                 </p>
               </div>
               <div>
-                <AddItem />
-                <ListItems />
+                {/* <AddItem />
+                <ListItems /> */}
               </div>
             </div>
           </div>
         </div>
       </FullpageSection>
       <FullpageSection>
-        <div>Section 2</div>
+        <div className='section-2 w-full'>
+          <div className='container mx-auto md:mx-24 px-5 md:px-0 my-32 md:my-60'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+              <div>
+                <p className='sub-heading'>My Skill</p>
+                <List unstyled>
+                  <List.Item>HTML</List.Item>
+                  <List.Item>CSS</List.Item>
+                  <List.Item>Jquery</List.Item>
+                  <List.Item>VueJs</List.Item>
+                  <List.Item>NextJs</List.Item>
+                  <List.Item>Wordpress</List.Item>
+                  <List.Item>Webflow</List.Item>
+                  <List.Item>Google Tag Manager</List.Item>
+                  <List.Item>SEO</List.Item>
+                </List>
+              </div>
+              <div>
+                <h3>About</h3>
+                <h2 className='sub-heading'>I'm Eko Bayu. A Frontend Developer, SEO Specialist and problem solver.</h2>
+                <p>
+                  I'm a passionate front-end developer with a keen understanding of SEO, dedicated to building websites
+                  that are both visually stunning and highly visible in search results. I combine my technical skills in
+                  HTML, CSS, JavaScript, and NextJS with my SEO knowledge to create user-friendly, engaging, and search
+                  engine-optimized websites that deliver results.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </FullpageSection>
       <FullpageSection>
         <div>Section 3</div>
