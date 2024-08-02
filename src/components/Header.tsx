4dd2d7e2b71a031a3f2d51521ba07325b1262ec3
Eko Bayu <eko.bayu@ogilvy.com>
2024-08-02 15:50:25 +0800

    padding menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,22 +9,22 @@ import styles from '../../styles/components/Header.module.scss'
 
 export default function Header() {
   return (
-    <Navbar className={`${styles.wrapperHeader} bg-transparent`}>
+    <Navbar className={`${styles.wrapperHeader} bg-transparent p-5`}>
       <Navbar.Brand as={Link} href='https://ekobayu.github.io'>
         <Image priority src={logo} alt='Eko Bayu Front End Web Developer' />
       </Navbar.Brand>
       <Navbar.Toggle />
-      <Navbar.Collapse className={`${styles.menuCollapse}`}>
-        <Navbar.Link href='#home' className={`${styles.menuItem} text-2xl mb-4 lg:mb-0`}>
+      <Navbar.Collapse className={`${styles.menuCollapse} mt-5`}>
+        <Navbar.Link href='#home' className={`${styles.menuItem} text-2xl mb-5 lg:mb-0`}>
           Home
         </Navbar.Link>
-        <Navbar.Link href='#about' className={`${styles.menuItem} text-2xl mb-4 lg:mb-0`}>
+        <Navbar.Link href='#about' className={`${styles.menuItem} text-2xl mb-5 lg:mb-0`}>
           About
         </Navbar.Link>
-        <Navbar.Link href='#work' className={`${styles.menuItem} text-2xl mb-4 lg:mb-0`}>
+        <Navbar.Link href='#work' className={`${styles.menuItem} text-2xl mb-5 lg:mb-0`}>
           Work
         </Navbar.Link>
-        <Navbar.Link href='#contact' className={`${styles.menuItem} text-2xl mb-4 lg:mb-0`}>
+        <Navbar.Link href='#contact' className={`${styles.menuItem} text-2xl mb-5 lg:mb-0`}>
           Contact
         </Navbar.Link>
       </Navbar.Collapse>
