14ea8d8d175d03c72e23ad3c09120242345a579f
Eko Bayu <eko.bayu@ogilvy.com>
2024-08-05 10:24:45 +0800

    add canonical and schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,26 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const structuredData = {
+    '@context': 'https://schema.org/',
+    '@type': 'Person',
+    name: 'Eko Bayu Kusumo',
+    url: 'https://ekobayu.com',
+    image: 'https://avatars.githubusercontent.com/u/4278034?v=4',
+    sameAs: 'https://github.com/ekobayu',
+    jobTitle: 'Front End Developer',
+    worksFor: {
+      '@type': 'Organization',
+      name: 'PT Bullseye'
+    }
+  }
+
   return (
     <html lang='en'>
       <head>
         <ThemeModeScript />
+        <link rel='canonical' href='https://ekobayu.com/' key='canonical' />
+        <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />
       </head>
       <body className={plusJakartaSans.className}>
         <Header />
